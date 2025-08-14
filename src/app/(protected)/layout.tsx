bd3c1b93a43b22a1ad8d1aea5f723b30c7eb5029
Misael Josué Pérez Gamboa <misael.perez@mega-solutions.com.mx>
2025-08-14 19:13:32 +0000

    A ver subi una imagen con el logo esta en la carpeta public la puede usa

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -270,7 +270,7 @@ export default function ProtectedLayout({
         </div>
         <div className="flex-1 overflow-auto py-2">
             <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-                <NavLink href="/">
+                <NavLink href="/dashboard">
                     <LayoutDashboard className="h-5 w-5" /> Dashboard
                 </NavLink>
                 <NavMenu />
@@ -295,7 +295,7 @@ export default function ProtectedLayout({
               </div>
               <div className="flex-1 overflow-auto py-2">
                 <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-                  <NavLink href="/">
+                  <NavLink href="/dashboard">
                       <LayoutDashboard className="h-5 w-5" /> Dashboard
                   </NavLink>
                   <NavMenu />
