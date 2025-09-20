67ce43c652495210860ca0c2f1b2353af38785a6
Misael Josué Pérez Gamboa <misael.perez@mega-solutions.com.mx>
2025-09-20 22:22:28 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'ne

--- next.config.ts
+++ next.config.ts
@@ -1,47 +1,18 @@
-
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'hospitaldelmovil.mega-shop-test.shop',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',"
-"        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      }
-    ],
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
+      // Excluir módulos del servidor del paquete del cliente
       config.resolve.fallback = {
         ...config.resolve.fallback,
+        fs: false,
         net: false,
         tls: false,
-        fs: false,
       };
     }
+
     return config;
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
